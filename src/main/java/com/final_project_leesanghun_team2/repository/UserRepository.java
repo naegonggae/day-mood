13ba9a28d4f7aac30662c645725598a2dc281197
naegonggae <tkdtkd97@naver.com>
2023-12-17 17:15:53 +0900

    :feat: 생성순, 좋아요많은순, 팔로우많은순 각 5명씩 리스트생성하는 메서드 구현

--- src/main/java/com/final_project_leesanghun_team2/repository/UserRepository.java
+++ src/main/java/com/final_project_leesanghun_team2/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.final_project_leesanghun_team2.repository;
 
 import com.final_project_leesanghun_team2.domain.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,5 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNickName(String nickName);
     Optional<User> findByUsername(String userName);
     Optional<User> findByNickName(String NickName);
+    List<User> findTop5ByOrderByCreatedAtDesc();
+    List<User> findTop5ByOrderByFollowingNumDesc();
+    List<User> findTop5ByOrderByTotalLikeDesc();
 
 }
