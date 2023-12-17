f1c0366f67ecac67b5c3f9db55a3c6eb5607e024
naegonggae <tkdtkd97@naver.com>
2023-12-17 20:56:57 +0900

    :fix: Tag Dto - request Valid 추가 - response 추가

--- src/main/java/com/final_project_leesanghun_team2/domain/dto/tag/TagFindResponse.java
+++ src/main/java/com/final_project_leesanghun_team2/domain/dto/tag/TagFindResponse.java
@@ -1,6 +1,5 @@
 package com.final_project_leesanghun_team2.domain.dto.tag;
 
-import com.final_project_leesanghun_team2.domain.entity.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +7,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class TagSaveResponse {
+public class TagFindResponse {
 
-	private Long id;
+	private boolean isExist;
 
-	public static TagSaveResponse from(Tag tag) {
-		return new TagSaveResponse(tag.getId());
+	public static TagFindResponse from(boolean isExist) {
+		return new TagFindResponse(isExist);
 	}
 }
--- src/main/java/com/final_project_leesanghun_team2/domain/dto/tag/TagSaveRequest.java
+++ src/main/java/com/final_project_leesanghun_team2/domain/dto/tag/TagSaveRequest.java
@@ -1,5 +1,6 @@
 package com.final_project_leesanghun_team2.domain.dto.tag;
 
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TagSaveRequest {
 
+	@NotBlank(message = "태그가 입력되지 않았습니다.")
 	private String name;
-
 }
