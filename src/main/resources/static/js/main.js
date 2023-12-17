94308e8a49744d163edd898e369e1583b68c9bf0
naegonggae <tkdtkd97@naver.com>
2023-12-17 13:33:51 +0900

    :feat: 전체 css, js 구현

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,432 @@
+'use strict';
+const baseUrl = 'http://localhost:8080';
+const token = getTokenFromCookie();
+
+// 홈 이동
+async function goLoginHome() {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const userId = result.result.id;
+      window.location.href = `${baseUrl}/${userId}`;
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 로그인
+async function login() {
+  const username = document.getElementById('loginUsername').value;
+  const password = document.getElementById('loginPassword').value;
+
+  const data = {
+    username : username,
+    password : password
+  };
+
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('로그인 성공!');
+      $('#loginModal').modal('hide');
+      window.location.href = `${baseUrl}/${result.result.id}`;
+    } else {
+      console.log('로그인 실패. HTTP 상태 코드:', response.status);
+      console.log('로그인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+  }
+}
+
+// 회원가입
+async function join() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+  const nickName = document.getElementById('nickName').value;
+
+  if (password !== confirmPassword) {
+    alert('비밀번호와 확인 비밀번호가 일치하지 않습니다.');
+    return;
+  }
+
+  const data = {
+    username : username,
+    password : password,
+    nickName : nickName
+  };
+
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/join', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('회원가입 성공!');
+      $('#signupModal').modal('hide');
+      window.location.href = baseUrl+'/';
+    } else {
+      console.log('회원가입 실패. HTTP 상태 코드:', response.status);
+      console.log('회원가입 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error during signup:', error);
+  }
+}
+
+// 쿠키 추출
+function getTokenFromCookie() {
+  const cookies = document.cookie.split(';');
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith('accessToken=')) {
+      // 'Authentication=' 이후의 문자열이 토큰입니다.
+      const token = cookie.substring('accessToken='.length);
+      return token;
+    }
+  }
+  return null;
+}
+
+// 내 상세 페이지 이동
+async function goToMyInfo() {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const userId = result.result.id;
+      window.location.href = `${baseUrl}/users/myInfo/${userId}`;
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 유저 상세 페이지 이동
+async function goToUserInfo(findUserId) {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const userId = result.result.id;
+      if (findUserId === userId) {
+        window.location.href = `${baseUrl}/users/myInfo/${findUserId}`;
+      } else {
+        window.location.href = `${baseUrl}/userInfo/${findUserId}/loginUser/${result.result.id}`;
+      }
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 피드백 제출
+async function feedBackSubmit() {
+  const title = document.getElementById("feedBackTitle").value;
+  const content = document.getElementById("feedBackContent").value;
+
+  const data = {
+    title: title,
+    content: content
+  };
+
+  try {
+    const response = await fetch('/api/v1/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('피드백 제출 성공:', result);
+      $('#feedBackModal').modal('hide');
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// 태그로 게시물 검색
+async function searchPost() {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const keyword = document.getElementById('searchInput').value.trim();
+      const userId = result.result.id;
+      try {
+        const response = await fetch(`${baseUrl}/api/v1/tags?tagName=${encodeURIComponent(keyword)}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+        const tagResult = await response.json();
+
+        if (tagResult.resultCode === 'SUCCESS') {
+
+          if (keyword === '') {
+            alert('키워드를 입력하세요.');
+            return false;
+          }
+
+          window.location.href = `${baseUrl}/search/${userId}?keyword=${encodeURIComponent(keyword)}`;
+          return false;
+        } else {
+          console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+          console.log('유저 확인 실패. 응답값:', tagResult);
+          alert("해당 태그의 게시물이 없습니다.");
+        }
+      } catch (error) {
+        console.error('유저 확인 에러 발생:', error.message.json());
+      }
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 설정 모달 열기
+async function openMyInfoModal() {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const userNickname = result.result.nickName;
+      const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+
+      document.getElementById('userNameSpan').innerText = userNickname;
+      myModal.show();
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 회원정보 수정
+async function updateMyInfo() {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const userId = result.result.id;
+      try {
+        const newNickname = document.getElementById('updateNickname').value;
+
+        const response = await fetch(`/api/v1/users/${userId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+          },
+          body: JSON.stringify({ nickName: newNickname }),
+        });
+        const result = await response.json();
+
+        if (result.resultCode === 'SUCCESS') {
+          console.log('회원 수정 성공:', result);
+          $('#myModal').modal('hide');
+
+        } else {
+          console.log('회원 수정 실패. HTTP 상태 코드:', response.status);
+          console.log('회원 수정 실패. 응답값:', result);
+          if (result && result.result && result.result.message !== undefined) {
+            alert(result.result.message);
+          } else {
+            alert(result.message);
+          }
+        }
+      } catch (error) {
+        console.error('회원 수정 에러 발생:', error);
+      }
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 유저 삭제
+async function deleteMyInfo() {
+  try {
+    const response = await fetch(baseUrl+'/api/v1/users/check', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      const userId = result.result.id;
+      try {
+        const confirmed = await showConfirmationDialog('정말로 계정을 삭제하시겠습니까?');
+
+        if (!confirmed) {
+          return;
+        }
+
+        await fetch(`/api/v1/users/${userId}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+          },
+        });
+
+        $('#myModal').modal('hide');
+        window.location.href = `${baseUrl}/`; // 홈페이지로 이동
+
+      } catch (error) {
+        console.error('회원 삭제 에러 발생:', error);
+      }
+    } else {
+      console.log('유저 확인 실패. HTTP 상태 코드:', response.status);
+      console.log('유저 확인 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('유저 확인 에러 발생:', error);
+  }
+}
+
+// 정말 삭제할지 경고창
+function showConfirmationDialog(message) {
+  return new Promise((resolve) => {
+    const confirmed = window.confirm(message);
+    resolve(confirmed);
+  });
+}
---
+++ src/main/resources/static/js/postTool.js
@@ -0,0 +1,269 @@
+// 좋아요
+async function pushLike(postId) {
+  try {
+    const response = await fetch(`${baseUrl}/api/v1/posts/${postId}/likes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('좋아요 이벤트 성공: ' + result);
+      const likedByMe = result.result.push;
+      const likesCount = result.result.count;
+
+      const likeIcon = document.getElementById(`likeButton-${postId}`);
+      const likeCountElement = document.querySelector(`.likeCount-${postId}`);
+
+      likeCountElement.innerText = likesCount;
+      likeIcon.className = `fa ${likedByMe ? 'fa-heart text-danger' : 'fa-heart-o'}`;
+    } else {
+      console.log('좋아요 이벤트 실패. HTTP 상태 코드:', response.status);
+      console.log('좋아요 이벤트 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('좋아요 이벤트 에러 발생:', error);
+  }
+}
+
+// 댓글 열기
+function toggleCommentForm(postId) {
+  const commentFormContainer = document.getElementById(`commentFormContainer-${postId}`);
+  commentFormContainer.style.display = commentFormContainer.style.display === 'none' ? 'block' : 'none';
+
+  if (commentFormContainer.style.display === 'block') {
+    loadComments(postId);
+  }
+}
+
+// 댓글 로드
+async function loadComments(postId) {
+  try {
+    const commentListContainer = document.getElementById(`commentList-${postId}`);
+    const commentFormContainer = document.getElementById(`commentFormContainer-${postId}`);
+
+    const response = await fetch(`${baseUrl}/api/v1/posts/${postId}/comments`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('댓글 로드 성공: '+result);
+      commentListContainer.innerHTML = '';
+
+      result.result.content.forEach(comment => {
+        const commentElement = createCommentElement(comment, postId);
+        commentListContainer.appendChild(commentElement);
+      });
+
+      // 댓글 보기 버튼을 누르면 다시 눌러서 숨기기까지 가능하도록
+      commentFormContainer.style.display = 'block';
+    } else {
+      console.log('댓글 로드 실패. HTTP 상태 코드:', response.status);
+      console.log('댓글 로드 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error during Comment request:', error);
+  }
+}
+function createCommentElement(comment, postId) {
+  const commentElement = document.createElement('div');
+  commentElement.innerHTML = `
+                    <div class="comment-${comment.id}" data-comment-id="1">
+                      <div class="cont-cmt-header">
+                        <div class="cont-cmt-user">
+                          <i onclick="goToUserInfo(${comment.userId})" class="fa fa-user-circle" aria-hidden="true"></i>
+                          <a onclick="goToUserInfo(${comment.userId})" class="nickName">${comment.nickName}</a>
+                          <a class="cmt-time-ago"> ${comment.updatedAt}</a>
+                        </div>
+                      </div>
+                      <div class="cmt-info">
+                        <p class="cmt-id">${comment.id}</p>
+                        <p>${comment.content}</p>
+                      </div>
+                      <a class="reply-btn" id="comment-${comment.id}" onclick="toggleReplyForm(${postId}, ${comment.id})">답글 보기</a>
+                      <div class="reply-set" id="replyFormContainer-${comment.id}" style="display: none;">
+                        <form class="d-flex">
+                          <div class="form-group flex-grow-1">
+                            <input class="form-control" id="replyContent-${comment.id}" name="reply" rows="5" placeholder="답글을 입력하세요." required></input>
+                          </div>
+                          <button type="button" class="btn btn-primary" onclick="addReply(${postId}, ${comment.id})">
+                            <i class="fa fa-angle-up" aria-hidden="true"></i>
+                          </button>
+                        </form>
+                        <div id="replyList-${comment.id}" class="reply-list">
+                          <!-- 여기에 답글 리스트가 들어갑니다 -->
+                        </div>
+                      </div>
+                    </div>
+    `;
+  return commentElement;
+}
+
+// 댓글 작성
+async function comment(postId) {
+  try {
+    const contentInput = document.getElementById(`commentContent-${postId}`);
+    const content = contentInput.value.trim();
+
+    if (content === '') {
+      alert('댓글 내용을 입력하세요.');
+      return;
+    }
+
+    const data = {
+      content : content,
+    };
+
+    const response = await fetch(baseUrl+'/api/v1/posts/'+postId+'/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('Comment 요청 성공:', result);
+      contentInput.value = '';
+      await loadComments(postId);
+    } else {
+      console.log('Comment 실패. HTTP 상태 코드:', response.status);
+      console.log('Comment 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error during Comment request:', error);
+  }
+}
+
+// 답글 열기
+function toggleReplyForm(postId, commentId) {
+  const replyFormContainer = document.getElementById(`replyFormContainer-${commentId}`);
+  replyFormContainer.style.display = replyFormContainer.style.display === 'none' ? 'block' : 'none';
+
+  if (replyFormContainer.style.display === 'block') {
+    loadReplies(postId, commentId);
+  }
+}
+
+// 답글 로드
+async function loadReplies(postId, commentId) {
+  try {
+    const replyFormContainer = document.getElementById(`replyFormContainer-${commentId}`);
+    const replyListContainer = document.getElementById(`replyList-${commentId}`);
+
+    const response = await fetch(`${baseUrl}/api/v1/posts/${postId}/comments/${commentId}/replies`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('답글 로드 성공:', result);
+      replyListContainer.innerHTML = '';
+      result.result.content.forEach(reply => {
+        const replyElement = createReplyElement(reply);
+        replyListContainer.appendChild(replyElement);
+      });
+
+      replyFormContainer.style.display = 'block';
+    } else {
+      console.log('답글 로드 실패. HTTP 상태 코드:', response.status);
+      console.log('답글 로드 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error during Reply request:', error);
+  }
+}
+function createReplyElement(reply) {
+  const replyElement = document.createElement('div');
+  replyElement.innerHTML = `
+                    <div class="reply-${reply.id}" data-reply-id="${reply.id}">
+                      <div class="cont-rp-header">
+                        <div class="cont-rp-user">
+                          <i onclick="goToUserInfo(${reply.userId})" class="fa fa-user-circle" aria-hidden="true"></i>
+                          <a onclick="goToUserInfo(${reply.userId})" class="nickName">${reply.nickName}</a>
+                          <a class="rp-time-ago"> ${reply.updatedAt}</a>
+                        </div>
+                      </div>
+                      <div class="rp-info">
+                        <p class="rp-id">${reply.id}</p>
+                        <p>${reply.content}</p>
+                      </div>
+                    <div>
+    `;
+  return replyElement;
+}
+
+// 답글 등록
+async function addReply(postId, commentId) {
+  try {
+    const replyContent = document.getElementById(`replyContent-${commentId}`);
+    const content = replyContent.value.trim();
+
+    if (content === '') {
+      alert('답글 내용을 입력하세요.');
+      return;
+    }
+
+    const data = {
+      content: content,
+    };
+
+    const response = await fetch(`${baseUrl}/api/v1/posts/${postId}/comments/${commentId}/replies`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    if (result.resultCode === 'SUCCESS') {
+      console.log('Reply 요청 성공:', result);
+      replyContent.value = '';
+      await loadReplies(postId, commentId);
+    } else {
+      console.log('Reply 실패. HTTP 상태 코드:', response.status);
+      console.log('Reply 실패. 응답값:', result);
+      if (result && result.result && result.result.message !== undefined) {
+        alert(result.result.message);
+      } else {
+        alert(result.message);
+      }
+    }
+  } catch (error) {
+    console.error('Error during Reply request:', error);
+  }
+}
