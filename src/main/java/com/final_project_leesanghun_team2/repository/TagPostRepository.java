687befe80ad402c869570b7203453b6ef88fd50d
naegonggae <tkdtkd97@naver.com>
2023-12-17 20:55:53 +0900

    :remove: Tag Repository - 사용하지 않는 메서드 삭제

--- src/main/java/com/final_project_leesanghun_team2/repository/TagPostRepository.java
+++ src/main/java/com/final_project_leesanghun_team2/repository/TagPostRepository.java
@@ -1,15 +1,12 @@
 package com.final_project_leesanghun_team2.repository;
 
-import com.final_project_leesanghun_team2.domain.entity.Post;
 import com.final_project_leesanghun_team2.domain.entity.Tag;
 import com.final_project_leesanghun_team2.domain.entity.TagPost;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagPostRepository extends JpaRepository<TagPost, Long> {
 
-	void deleteByTagAndPost(Tag tag, Post post);
 	Page<TagPost> findAllByTag(Tag tag, Pageable pageable);
 }
--- src/main/java/com/final_project_leesanghun_team2/repository/TagRepository.java
+++ src/main/java/com/final_project_leesanghun_team2/repository/TagRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
 	boolean existsByName(String tagName);
-
 	Optional<Tag> findByName(String tagName);
 
 }
