863559a651ff04cac471d9eefa12852f5eb0f422
naegonggae <tkdtkd97@naver.com>
2023-12-17 17:23:42 +0900

    :fix: User 수정 - username, nickName 필드길이 수정 - 좋아요 필드 추가 - 좋아요 증감 메서드 추가

--- src/main/java/com/final_project_leesanghun_team2/domain/entity/User.java
+++ src/main/java/com/final_project_leesanghun_team2/domain/entity/User.java
@@ -1,6 +1,6 @@
 package com.final_project_leesanghun_team2.domain.entity;
 
-import com.final_project_leesanghun_team2.domain.dto.user.UserJoinRequest;
+import com.final_project_leesanghun_team2.domain.dto.user.request.UserJoinRequest;
 import lombok.*;
 
 import javax.persistence.*;
@@ -14,11 +14,11 @@ public class User extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(nullable = false, unique = true, length = 320)
     private String username; // 이메일
     @Column(nullable = false)
     private String password;
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(nullable = false, unique = true, length = 15)
     private String nickName;
     @Enumerated(EnumType.STRING)
     private UserRole role;
@@ -26,7 +26,8 @@ public class User extends BaseEntity {
     private Long followNum;
     @Column(nullable = false)
     private Long followingNum;
-    // 상태 메세지 정보
+    @Column(nullable = false)
+    private Long totalLike;
 
     // 생성 메서드
     public static User createUser(UserJoinRequest request, String encodedPW) {
@@ -39,6 +40,7 @@ public class User extends BaseEntity {
         this.role = UserRole.USER;
         this.followNum = 0L;
         this.followingNum = 0L;
+        this.totalLike = 0L;
     }
 
     // 수정 메서드
@@ -49,20 +51,28 @@ public class User extends BaseEntity {
     // 팔로우 감소 메서드
     public void decreaseFollowingNum() {
         this.followingNum--;
-        // 상대방 followNum 을 컨트롤 해줘야해
+        // 상대방 followNum 을 컨트롤
     }
     public void decreaseFollowNum() {
         this.followNum--;
-        // 상대방 followNum 을 컨트롤 해줘야해
+        // 상대방 followNum 을 컨트롤
     }
 
     // 팔로우 증가 메서드
     public void increaseFollowingNum() {
         this.followingNum++;
-        // 상대방 followNum 을 컨트롤 해줘야해
+        // 상대방 followNum 을 컨트롤
     }
     public void increaseFollowNum() {
         this.followNum++;
-        // 상대방 followNum 을 컨트롤 해줘야해
+        // 상대방 followNum 을 컨트롤
+    }
+
+    // 좋아요 증감 메서드
+    public void increaseTotalLike() {
+        this.totalLike++;
+    }
+    public void decreaseTotalLike() {
+        this.totalLike--;
     }
 }
