42ca7fdd4fa6411908d608025d37f52e5041571b
naegonggae <tkdtkd97@naver.com>
2023-12-17 21:16:46 +0900

    :fix: comment Repository 파라미터명 변경

--- src/main/java/com/final_project_leesanghun_team2/repository/CommentRepository.java
+++ src/main/java/com/final_project_leesanghun_team2/repository/CommentRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByPostAndParentNull(Post post, Pageable pageable);
-    Page<Comment> findAllByPostAndParent(Post post, Comment comment, Pageable pageable);
+    Page<Comment> findAllByPostAndParent(Post post, Comment parentComment, Pageable pageable);
 
 }
