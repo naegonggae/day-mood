788b85e5f070cb0a1d30ce89492539612afd9a91
naegonggae <tkdtkd97@naver.com>
2023-12-17 20:24:17 +0900

    :feat: User의 게시물 개수를 확인하는 메서드 선언

--- src/main/java/com/final_project_leesanghun_team2/repository/PostRepository.java
+++ src/main/java/com/final_project_leesanghun_team2/repository/PostRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByUser(User user, Pageable pageable);
+    Long countAllByUser(User user);
 }
