22d90550d14bbbc23e9edaad98521863d3eb6538
naegonggae <tkdtkd97@naver.com>
2023-12-19 12:33:30 +0900

    :fix: 배포 테스트

--- src/main/java/com/final_project_leesanghun_team2/configuration/SecurityConfig.java
+++ src/main/java/com/final_project_leesanghun_team2/configuration/SecurityConfig.java
@@ -50,12 +50,12 @@ public class SecurityConfig {
                 // 에러 핸들링
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler).and()
+                .accessDeniedHandler(jwtAccessDeniedHandler).and();
 
                 // 인증필터 적용
 //                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 // 인증과정에서 발생한 에러 핸들링
-                .addFilterBefore(jwtExceptionFilter, JwtAuthorizationFilter.class);
+//                .addFilterBefore(jwtExceptionFilter, JwtAuthorizationFilter.class);
 
         return http.build();
     }
